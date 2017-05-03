125e92abfc9aed48f91e9a2f8d942bf9ad614209
Victor Guillen <victorguillen@Victors-MacBook-Air.local>
2017-05-02 20:13:04 -0700

    testing production config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -21,6 +21,18 @@ module.exports = {
       }
     ]
   },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx" ]
